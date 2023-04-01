90f1b02aa4637ed1219afd9d5834329d05975cc5
Willomadiel <willogameplay16@gmail.com>
2023-04-01 07:57:04 -0300

    att

---
+++ index.js
@@ -0,0 +1,21 @@
+function login(){
+    if (log.style.display == 'block' || itens.style.display == 'block') {
+        itens.style.display = 'none'
+        log.style.display = 'none'
+    } else {
+        log.style.display = 'block'
+    }
+}
+function clickMenu(){
+    if (itens.style.display == 'block' || log.style.display == 'block') {
+        itens.style.display = 'none'
+        log.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+
+}
+var proc = document.getElementById('procura')
+function procura(){
+    proc.localName()
+} 
