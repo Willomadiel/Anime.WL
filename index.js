3ee1ee6751ebc9905e793eea237cf061e78447cf
Willomadiel Costa <Willogameplay16@gmail.com>
2023-05-30 09:20:48 -0300

    att

--- index.js
+++ index.js
@@ -14,35 +14,20 @@ function clickMenu(){
         itens.style.display = 'block'
     }
 }
-var lista = []
 function procuras(){
-    var procura = document.getElementById('procura')
-    var valor = procura.value
+    const procura = document.getElementById('procura')
+    const valor = procura.value
     if (valor == "" || isNaN(valor) == false){
         window.location.href='Erro.html'
+        lista = valor
         return;
     }
-    window.location.href='alfabeto/all.html'
+    window.location.href='resposta.html'
 } 
 var procurase = document.getElementById('procurase')
-procurase.addEventListener('click', procuras)
-function procurando(){
-    if (canalha.style.display == 'block'){
-        canalha.style.display = 'none'
-    }else{
-        canalha.style.display = 'block'
-    }
-}
-var procura = document.getElementById('procura')
-procura.addEventListener('click', procurando)
+procurase.addEventListener('click', procuras);
 
-function buscar(){
-    var canalha = document.getElementById('canalha')
-    if (canalha.style.display == 'block'){
-        canalha.style.display = 'none'
-    }else{
-        canalha.style.display = 'block'
-    }
-}
-var cana = document.getElementsByClassName('li')
-cana.addEventListener('click', buscar)
+
+
+
+  
